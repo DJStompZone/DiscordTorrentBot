a3eee347a5f0efe55fd4181768fa9b24e11a03d7
Jon <jondbooth@gmail.com>
2020-12-29 22:35:15 +1300

    Add files via upload

--- bot.py
+++ bot.py
@@ -6,7 +6,6 @@ import configparser
 from transmission_rpc import Client
 from discord.ext import commands
 
-
 def configParserToDict(config_file_location):
     parser = configparser.ConfigParser()
     parser.read('DiscordTorrentBot.cfg')    
@@ -18,22 +17,32 @@ def configParserToDict(config_file_location):
             config_dict[element][name] = value    
             
     return config_dict
-    
-config = configParserToDict('DiscordTorrentBot.cfg')
 
+client = discord.Client()
+bot = commands.Bot(command_prefix='!', case_insensitive=True)
+
+
+
+config = configParserToDict('DiscordTorrentBot.cfg')
+current_torrents = []
 transmission_settings = config['TransmissionServer']
 jackett_settings = config['JackettServer']
 discord_settings = config['DiscordSettings']
 general_settings = config['General']
+TVpath = general_settings['tvlocation']
+Animepath = general_settings['animelocation']
+Moviepath =  general_settings['movieslocation']
+
+
 
 emojis = ["1️⃣","2️⃣","3️⃣","4️⃣","5️⃣"]
 
 jackett_APIkey = jackett_settings['apikey']
 discord_bot_api= discord_settings['apikey']
-client = discord.Client()
 c = Client(host=transmission_settings['ip'], port=transmission_settings['port'], username=transmission_settings['username'], password=transmission_settings['password'])
-bot = commands.Bot(command_prefix='!', case_insensitive=True)
-
+ 
+    
+    
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
@@ -51,7 +60,7 @@ async def downloadAnime(ctx, show, *args):
     if ctx.channel.name == "plex-torrents":
         for x in args:
             show += " " + x
-        #c.add_torrent(torrent_url, download_dir='/data/downloads/TV/')
+
         show = show.replace(" ", "+")
         show = "https://nyaa.si/?page=rss&q=" + str(show) + "&c=1_2&f=0"
         
@@ -63,25 +72,10 @@ async def downloadAnime(ctx, show, *args):
             for i in range(min(5,len(feedsorted))):
                 msg += "```" + str(i+1) +") " + str(feedsorted[i].title) + " | " + str(feedsorted[i].nyaa_size) + " | seeders : " + str(feedsorted[i].nyaa_seeders) + "```"
                            
-            message = await ctx.send(msg)
+            selection = await postMessageAndAwaitReaction(ctx, msg, min(5,len(feedsorted)))
             
-            for i in range(min(5,len(feedsorted))):
-                await message.add_reaction(emojis[i])
-                
-            selection=0
-            for x in range(20):
-                message = await ctx.fetch_message(message.id)
-                x=1
-                for reaction in message.reactions:            
-                    if reaction.count==2:
-                        selection=x
-                        break
-                    x = x+1
-                if selection != 0:
-                    break
-                await asyncio.sleep(1)
             if selection != 0:
-                c.add_torrent(feedsorted[selection-1].link, download_dir='/data/downloads/Anime/')
+                add_torrent(feedsorted[selection-1].link, Animepath)
                 await ctx.send("Torrent " + str(feedsorted[selection-1].title) + " added")
             else:
                 await ctx.send("Selection timed out, try again loser boy")
@@ -95,10 +89,9 @@ async def downloadTV(ctx, show, *args):
     if ctx.channel.name == "plex-torrents":
         for x in args:
             show += " " + str(x)
-        #c.add_torrent(torrent_url, download_dir='/data/downloads/TV/')
+
         show = show.replace(" ", "+")
         show = "http://" + jackett_settings['ip'] + ":" + jackett_settings['port'] + "/api/v2.0/indexers/thepiratebay/results/torznab/api?apikey=" + jackett_APIkey + "&t=search&cat=&q=" + str(show)
-        print(show)
         feed = feedparser.parse(show)
         msg= "Found the below torrents, select an option via reactions:"
         if(len(feed.entries) > 0):
@@ -107,25 +100,10 @@ async def downloadTV(ctx, show, *args):
             for i in range(min(5,len(feedsorted))):
                 msg += "```" + str(i+1) +") " + str(feedsorted[i].title) + " | " + sizeof_fmt(int(feedsorted[i].size)) + "```"
                            
-            message = await ctx.send(msg)
+            selection = await postMessageAndAwaitReaction(ctx, msg, min(5,len(feedsorted)))
             
-            for i in range(min(5,len(feedsorted))):
-                await message.add_reaction(emojis[i])
-                
-            selection=0
-            for x in range(20):
-                message = await ctx.fetch_message(message.id)
-                x=1
-                for reaction in message.reactions:            
-                    if reaction.count==2:
-                        selection=x
-                        break
-                    x = x+1
-                if selection != 0:
-                    break
-                await asyncio.sleep(1)
             if selection != 0:
-                c.add_torrent(feedsorted[selection-1].link, download_dir='/data/downloads/TV/')
+                add_torrent(feedsorted[selection-1].link, TVpath)
                 await ctx.send("Torrent " + str(feedsorted[selection-1].title) + " added")
             else:
                 await ctx.send("Selection timed out, try again loser boy")
@@ -150,25 +128,10 @@ async def downloadMovie(ctx, show, *args):
             for i in range(min(5,len(feedsorted))):
                 msg += "```" + str(i+1) +") " + str(feedsorted[i].title) + " | " + sizeof_fmt(int(feedsorted[i].size)) + "```"
                            
-            message = await ctx.send(msg)
-            
-            for i in range(min(5,len(feedsorted))):
-                await message.add_reaction(emojis[i])
+            selection = await postMessageAndAwaitReaction(ctx, msg, min(5,len(feedsorted)))
                 
-            selection=0
-            for x in range(20):
-                message = await ctx.fetch_message(message.id)
-                x=1
-                for reaction in message.reactions:            
-                    if reaction.count==2:
-                        selection=x
-                        break
-                    x = x+1
-                if selection != 0:
-                    break
-                await asyncio.sleep(1)
             if selection != 0:
-                c.add_torrent(feedsorted[selection-1].link, download_dir='/data/downloads/Movies/')
+                add_torrent(feedsorted[selection-1].link, Moviepath)
                 await ctx.send("Torrent " + str(feedsorted[selection-1].title) + " added")
             else:
                 await ctx.send("Selection timed out, try again loser boy")
@@ -177,7 +140,44 @@ async def downloadMovie(ctx, show, *args):
     else:
         await ctx.send("You must use the plex-torrents chat")
                 
-
+@bot.command(name='eta', help='Shows ETA of current shows downloading.')
+async def eta(ctx):
+    msg = "Torrents remaining:"
+    for x in current_torrents:
+        tor = c.get_torrent(x)
+        if(tor.status != "stopped") :
+            msg+="```"+tor.id() + " : " + tor.format_eta() + "```"
+        else:
+            current_torrents.remove(x)
+    await ctx.send(msg)
+        
+                
+async def postMessageAndAwaitReaction(ctx, msg, numberofemojis):
+    message = await ctx.send(msg)
+    
+    for i in range(numberofemojis):
+        await message.add_reaction(emojis[i])
+            
+    selection=0
+    for x in range(20):
+        message = await ctx.fetch_message(message.id)
+        x=1
+        for reaction in message.reactions:            
+            if reaction.count==2:
+                selection=x
+                break
+            x = x+1
+        if selection != 0:
+            break
+        await asyncio.sleep(1)    
+    return selection
+   
+def add_torrent(torrent, path1):
+    print(path1)
+    torrent = c.add_torrent(torrent, download_dir=str(path1))
+    current_torrents.append(torrent.id)
+    
+    
 def sizeof_fmt(num, suffix='B'):
     for unit in ['','Ki','Mi','Gi','Ti','Pi','Ei','Zi']:
         if abs(num) < 1024.0:
@@ -185,15 +185,7 @@ def sizeof_fmt(num, suffix='B'):
         num /= 1024.0
     return "%.1f%s%s" % (num, 'Yi', suffix)
 
-def configParserToDict(config_file_location):
-    parser = configparser.ConfigParser()
-    parser.read('DiscordTorrentBot.cfg')    
-    config_dict = {}
     
-    for element in parser.sections():
-        config_dict[element] = {}
-        for name, value in parser.items(element):
-            config_dict[element][name] = value    
 
-bot.run(discord_bot_api)
 
+bot.run(discord_bot_api)   
